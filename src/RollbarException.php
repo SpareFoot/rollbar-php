9933dd45592a70d738d1e80db27631fb7608b2c5
Rex McConnell <rmcconnell@sparefoot.com>
2016-09-22 16:45:01 -0500

    PHP7ize

--- src/RollbarException.php
+++ src/RollbarException.php
@@ -5,7 +5,7 @@ class RollbarException {
     private $message;
     private $exception;
 
-    public function __construct($message, \Exception $exception = null) {
+    public function __construct($message, \Throwable $exception = null) {
         $this->message = $message;
         $this->exception = $exception;
     }
